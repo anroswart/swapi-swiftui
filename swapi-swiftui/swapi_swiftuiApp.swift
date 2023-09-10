c8a4045ba207e1ab44cbdfe396a982ab6c69bc72
Anro Swart <anro.swart@entelect.co.za>
2023-09-10 16:42:08 +0200

    Initial Commit

---
+++ swapi-swiftui/swapi_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swapi_swiftuiApp.swift
+//  swapi-swiftui
+//
+//  Created by Anro Swart on 2023/09/10.
+//
+
+import SwiftUI
+
+@main
+struct swapi_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
