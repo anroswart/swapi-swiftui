2a004a6ee37991817261215cd036afed05315fa9
Anro Swart <anro.swart@entelect.co.za>
2023-09-14 09:59:55 +0200

    Implement opening crawl

--- swapi-swiftui/lib/views/detail-views/FilmDetailView.swift
+++ swapi-swiftui/lib/views/detail-views/FilmDetailView.swift
@@ -10,49 +10,67 @@ import SwiftUI
 struct FilmDetailView: View {
     @EnvironmentObject var router: RouterService
     let film: Film
+    @State private var yOffset: CGFloat = 540
+    private let scrollDistance: CGFloat = 720
     private let placeholderImage = UIImage(systemName: "exclamationmark.triangle")!
     
     var body: some View {
+        ZStack(alignment: .top) {
+            infoCard
+                .padding(EdgeInsets(top: 160, leading: 16, bottom: 40, trailing: 16))
+                .zIndex(1)
+            
+            openingCrawl
+                .zIndex(0)
+        }
+        .navigationBarBackButtonHidden(true)
+        .background {
+            Image("background")
+                .scaledToFit()
+                .ignoresSafeArea()
+        }
+    }
+    
+    private var infoCard: some View {
         ZStack(alignment: .topTrailing) {
-            RoundedRectangle(cornerRadius: 10)
-                .fill(Color.gray)
-                .shadow(radius: 5)
-                .overlay(
-                    VStack(spacing: 10) {
-                        Image(uiImage: UIImage(data: film.posterData ?? Data()) ?? placeholderImage)
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(height: 150)
-                            .cornerRadius(10)
-                        
-                        Text(film.title ?? "TBA")
-                            .font(.headline)
-                            .multilineTextAlignment(.center)
-                            .lineLimit(2)
-                            .padding(.horizontal, 10)
-                        
-                        Text("Release Date: \(film.releaseDate ?? "")")
-                            .font(.subheadline)
-                        
-                        Text("Directors: \(film.director ?? "")")
-                            .font(.subheadline)
-                        
-                        Text("Producers: \(film.producer ?? "")")
-                            .font(.subheadline)
-                        
-                        Button {
-                            router.push(
-                                CharactersView(characters: film.characterList ?? ["No data"])
-                                    .environmentObject(router)
-                            )
-                        } label: {
-                            Text("View film characters")
-                                .foregroundColor(Color("bananna-yellow"))
-                        }
-                    }
-                        .padding()
-                )
-                .padding(EdgeInsets(top: 40, leading: 16, bottom: 40, trailing: 16))
+            VStack(spacing: 10) {
+                Image(uiImage: UIImage(data: film.posterData ?? Data()) ?? placeholderImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 160)
+                    .cornerRadius(10)
+                
+                Text(film.title ?? "TBA")
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .padding(.horizontal, 8)
+                
+                Text("Release Date: \(film.releaseDate ?? "")")
+                    .font(.subheadline)
+                
+                Text("Directors: \(film.director ?? "")")
+                    .font(.subheadline)
+                
+                Text("Producers: \(film.producer ?? "")")
+                    .font(.subheadline)
+                
+                Button {
+                    router.push(
+                        CharactersView(characters: film.characterList ?? ["No data"])
+                            .environmentObject(router)
+                    )
+                } label: {
+                    Text("View film characters")
+                        .foregroundColor(Color("bananna-yellow"))
+                }
+                .padding(.bottom)
+            }
+            .background {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.gray)
+                    .shadow(radius: 5)
+            }
             
             Button(action: {
                 router.pop()
@@ -62,14 +80,28 @@ struct FilmDetailView: View {
                     .foregroundColor(.red)
                     .padding()
             }
-            .padding(EdgeInsets(top: 56, leading: 0, bottom: 0, trailing: 32))
+            .padding()
         }
-        .navigationBarBackButtonHidden(true)
-        .background {
-            Image("background")
-                .scaledToFit()
-                .ignoresSafeArea()
+    }
+    
+    private var openingCrawl: some View {
+        let leadingSpace = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+        return VStack {
+            Text("\(leadingSpace)\(film.openingCrawl ?? "")")
+                .fontWeight(.bold)
+                .font(.subheadline)
+                .foregroundColor(Color("bananna-yellow"))
+                .multilineTextAlignment(.center)
+                .lineSpacing(8)
+                .fixedSize(horizontal: false, vertical: true)
+                .frame(maxHeight: .infinity)
+                .offset(y: yOffset)
+                .animation(.linear(duration: 45).repeatForever(autoreverses: false), value: UUID())
+                .onAppear {
+                    self.yOffset = -scrollDistance
+                }
         }
+        .rotation3DEffect(.degrees(60), axis: (x: 1, y: 0, z: 0))
     }
 }
 
